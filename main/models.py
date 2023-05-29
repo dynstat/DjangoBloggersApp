d130c02a998e1dd142e431be0f4e2818960cf7cf
dynstat <vs15vivek@gmail.com>
2023-05-29 21:54:36 +0530

    added last_login and is_expired field in User modal created Blog model

--- main/models.py
+++ main/models.py
@@ -9,4 +9,11 @@ class User(models.Model):
     email = models.EmailField()
     session_id = models.CharField(max_length=100, default=0)
     time_stamp = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(default=timezone.now)
+    is_expired = models.BooleanField(default=False)
     email_verification = models.BooleanField(default=0)
+
+class Blog(models.Model):
+    rel_user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    title = models.TextField()
+    content= models.TextField()
